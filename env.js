4dbb015245f3f637a613d416d8d5c795bb6563c7
vinnicirne <vinnicirne@users.noreply.github.com>
2026-09-12 18:23:03 -0300

    build: atualiza env.js

--- env.js
+++ env.js
@@ -1,10 +1,10 @@
 // Arquivo de configuração de ambiente gerado automaticamente
 // Não modifique manualmente. Altere o arquivo .env ou as variáveis na Vercel.
 window.__ENV__ = {
-  NEXT_PUBLIC_SUPABASE_URL: "",
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: "",
-  NEXT_SUPABASE_URL: "",
-  NEXT_SUPABASE_KEY: "",
-  SUPABASE_URL: "",
-  SUPABASE_KEY: ""
+  NEXT_PUBLIC_SUPABASE_URL: "https://xlxuwqcszhxxzofebkjb.supabase.co",
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseHV3cWNzemh4eHpvZmVia2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNjUxNzYsImV4cCI6MjEwNDY0MTE3Nn0.ME_Zo11tBK-U5CuYnLtAGvwuok-YZFcPfuLbaZxiVjE",
+  NEXT_SUPABASE_URL: "https://xlxuwqcszhxxzofebkjb.supabase.co",
+  NEXT_SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseHV3cWNzemh4eHpvZmVia2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNjUxNzYsImV4cCI6MjEwNDY0MTE3Nn0.ME_Zo11tBK-U5CuYnLtAGvwuok-YZFcPfuLbaZxiVjE",
+  SUPABASE_URL: "https://xlxuwqcszhxxzofebkjb.supabase.co",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseHV3cWNzemh4eHpvZmVia2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNjUxNzYsImV4cCI6MjEwNDY0MTE3Nn0.ME_Zo11tBK-U5CuYnLtAGvwuok-YZFcPfuLbaZxiVjE"
 };
