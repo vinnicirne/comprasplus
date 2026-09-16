4086b0b81d1e7dcef530db4152bbeb02f952e5c7
vinnicirne <vinnicirne@users.noreply.github.com>
2026-09-15 22:58:32 -0300

    fix: oculta splash screen nativa para evitar travamento

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './core/styles/global.css'
 import App from './App.tsx'
+import { SplashScreen } from '@capacitor/splash-screen';
+
+// Oculta a splash screen nativa para deixar a nossa animada (React) brilhar!
+SplashScreen.hide().catch(() => {});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
